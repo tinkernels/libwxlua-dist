7b5f7c4a83b31abbf84d01016b1da059dd3076dc
tinkernels <don.johnny.cn@gmail.com>
2022-11-17 07:39:33 +0800

    Fix issue of packing wrong files.

--- release-windows.cmd
+++ release-windows.cmd
@@ -3,7 +3,7 @@ chcp 65001 >NUL 2>NUL
 
 cd /d %~dp0
 
-cmd /c 0-fetch-deps.cmd
+@REM cmd /c 0-fetch-deps.cmd
 cmd /c 1-build-wxwidgets.cmd
 cmd /c 2-build-wxlua.cmd
 cmd /c 3-clean-4release.cmd
@@ -13,4 +13,4 @@ cmd /c 3-clean-4release.cmd
 7z a wxlua-dist-winx64.7z build-wxlua\x64
 7z a wxlua-dist-win32.7z build-wxlua\x86
 7z a wxWidgets-dist-winx64.7z build-wxWidgets\x64
-7z a wxWidgets-dist-win32.7z build-wxWidgets\x64
+7z a wxWidgets-dist-win32.7z build-wxWidgets\x86
