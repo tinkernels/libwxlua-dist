cf006675eeb26f1ad6b134b18f36acc80b8c6f50
tinkernels <don.johnny.cn@gmail.com>
2022-12-18 02:22:54 +0800

    (ci:release) v3.2.0.2-build.8
    Refine release shell for cirrus ci.

--- release-macos-arm64.sh
+++ release-macos-arm64.sh
@@ -24,6 +24,14 @@ unset ORIGINAL_PWD_GETSELFPATHVAR SH_FILE_RUN_PATH_GETSELFPATHVAR SH_FILE_RUN_BA
 
 cd "$SH_SELF_PATH_DIR_RESULT" || exit
 
+# Originally for cirrus ci, build only when recent tag exists.
+GIT_MOST_RECENT_TAG=$(git describe --tags --abbrev=0 "$(git rev-list --tags --max-count=1)")
+if [ -n "$GIT_MOST_RECENT_TAG" ];then
+    echo "GIT_MOST_RECENT_TAG: $GIT_MOST_RECENT_TAG"
+else
+    echo "No tag found, skip job." && exit
+fi
+
 brew update; brew upgrade
 
 bash ./0-fetch-deps-macos-arm64.sh || exit
@@ -43,10 +51,11 @@ tar -cvf "$TK_wxWidgets_RELEASE_TARBALL" wxWidgets-dist
 TK_wxLua_RELEASE_TARBALL="wxlua-dist-macos-arm64.tar.gz"
 tar -cvf "$TK_wxLua_RELEASE_TARBALL" wxlua-dist
 
-if [[ "$CIRRUS_RELEASE" == "" ]]; then
-  echo "Not a release. No need to deploy!"
-  exit 0
-fi
+FILES_TO_UPLOAD=(
+  "$TK_LUAJIT_RELEASE_TARBALL"
+  "$TK_wxWidgets_RELEASE_TARBALL"
+  "$TK_wxLua_RELEASE_TARBALL"
+)
 
 if [[ "$GITHUB_TOKEN" == "" ]]; then
   echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
@@ -54,20 +63,48 @@ if [[ "$GITHUB_TOKEN" == "" ]]; then
 fi
 
 FILE_CONTENT_TYPE="application/octet-stream"
-FILES_TO_UPLOAD=(
-  "$TK_LUAJIT_RELEASE_TARBALL"
-  "$TK_wxWidgets_RELEASE_TARBALL"
-  "$TK_wxLua_RELEASE_TARBALL"
+
+function get_release_id_of_git_tag {
+    JSON_GITHUB_RELEASES_=$(curl -sSfL \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases")
+
+    [ -n "$JSON_GITHUB_RELEASES_" ] || exit
+
+    PY_CODE_=$(cat <<EOF
+import json
+import sys
+try:
+    j_ = json.load(sys.stdin)
+    for release_ in j_:
+        if release_["tag_name"] == "$1":
+            print(release_["id"])
+except (Exception,):
+    pass
+EOF
 )
+    echo "$JSON_GITHUB_RELEASES_" | python3 -c "$PY_CODE_"
+}
+
+# Loop until github release for current 
+GITHUB_RELEASE_ID_=""
+while true
+do
+    GITHUB_RELEASE_ID_=$(get_release_id_of_git_tag "$GIT_MOST_RECENT_TAG")
+    [ -n "$GITHUB_RELEASE_ID_" ] && break
+    sleep 60
+done
 
 for FPATH in "${FILES_TO_UPLOAD[@]}"
 do
-  echo "Uploading $FPATH..."
-  NAME=$(basename "$FPATH")
-  URL_TO_UPLOAD="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$NAME"
-  curl -X POST \
-    --data-binary @"$FPATH" \
-    --header "Authorization: token $GITHUB_TOKEN" \
-    --header "Content-Type: $FILE_CONTENT_TYPE" \
-    "$URL_TO_UPLOAD"
+    echo "Uploading $FPATH..."
+    NAME=$(basename "$FPATH")
+    URL_TO_UPLOAD="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$GITHUB_RELEASE_ID_/assets?name=$NAME"
+    curl -X POST \
+        --data-binary @"$FPATH" \
+        --header "Authorization: token $GITHUB_TOKEN" \
+        --header "Content-Type: $FILE_CONTENT_TYPE" \
+        "$URL_TO_UPLOAD"
 done
